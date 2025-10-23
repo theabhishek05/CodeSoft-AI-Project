2f0e6c8a5533bb0d74b90728d26dd3a62348b5a5
theabhishek05 <abhishekkr62762>
2025-10-23 14:38:57 +0530

    Fixing some issues Final commit

--- Recommendation System/RecommendationSystem.java
+++ Recommendation System/RecommendationSystem.java
@@ -1,43 +1,52 @@
 import java.util.Scanner;
-public class RecommendationSystem {
-    public static void main(String[]arga){
-        String [] Fantasy = {"Harry Potter , Pirates of caribbean , The Hobbit , Lord of the rings"};
-        String[] Scifi  = {"Lucy , Interstellar , Inception , oppenheimer , The Martian"};
-        String[] Horror = {"The conjuring , Sinister , It , Hereditary, Haunted "};
-        String [] Biography = {" Sanju , The wolf of wall street , Ford Vs Ferrari ,The Pursuit of happiness"};
-        String [] Love = {"kal ho na ho , Barfi , Raanjhanaa, Aashiqui 2, Saiyaara"};
+public class RecommendationSystem{
+    public static void main(String[] arga){
+        String[] Fantasy= {"Harry Potter , Pirates of Caribbean ,The Hobbit , Lord of the Rings"};
+        String[] Scifi ={"Lucy , Interstellar ,Inception , Oppenheimer , The Martian"};
+        String[] Horror = {"The.Conjuring , Sinister , It , Hereditary , Haunted"};
+        String[] Biography= {"Sanju ,The Wolf of Wall Street , Ford Vs Ferrari , The Pursuit of Happiness"};
+        String[] Love ={"Kal Ho Na Ho , Barfi , Raanjhanaa , Aashiqui 2 ,Saiyaara"};
         Scanner sc = new Scanner(System.in);
-        System.out.println("What kind of movie do you like ? (Fantasy/Scifi/Horror/Biography,Love)");
-        String Genre = sc.nextLine();
 
-        if (Genre.equalsIgnoreCase("Fanstasy")){
-            System.out.println("Recommended Fantasy Movie:");
-            for (String movie : Fantasy){
-                System.out.println(movie);
+        while(true){
+            System.out.println("What kind of movies are looking for ?  (Fantasy/Scifi/Horror/Biography/Love) Type 'Exit' to quit");
+            String Genre= sc.nextLine();
+            if(Genre.equalsIgnoreCase("Exit")){
+                System.out.println("Thank You ! GoodBye ");
+                break;
             }
-        }else if(Genre.equalsIgnoreCase("Scifi")){
-            System.out.println("Recommended SciFi Movies :");
-            for(String movie : Scifi){
-                System.out.println(movie);
-            }
+            if(Genre.equalsIgnoreCase("Fantasy")){
+                System.out.println("Recommended Fantasy Movie:");
+                for (String movie : Fantasy){
+                    System.out.println(movie);
+                 }
+            }else if (Genre.equalsIgnoreCase("Scifi")){
+                System.out.println("Recommended SciFi Movies:");
+                for (String movie : Scifi){
+                    System.out.println(movie);
+                }
 
-        }else if (Genre.equalsIgnoreCase("Horror")){
-            System.out.println("Recommemded Horror Movies :");
-            for(String movie : Horror){
-                System.out.println(movie);
-            }
-        }else if (Genre.equalsIgnoreCase("Biography")){
-            System.out.println("Recommeded Biography Movies");
-            for(String movie : Biography){
-                System.out.println(movie);
-            }
-        }else if (Genre.equalsIgnoreCase("Love")){
-            System.out.println("Recommended Love Movies ");
-            for(String movie : Love){
-                System.out.println(movie);
-            }
-        }else{
-            System.out.println("Sorry , No Recommendations availablr for that genre .Please choose of the given Recommendation.");
+            }else if(Genre.equalsIgnoreCase("Horror")) {
+                System.out.println("Recommemded Horror Movies :");
+                for(String movie : Horror){
+                    System.out.println(movie);
+                }
+            }else if(Genre.equalsIgnoreCase("Biography")){
+                System.out.println("Recommeded Biography Movies");
+                for (String movie : Biography){
+                 System.out.println(movie);
+                }
+            }else if (Genre.equalsIgnoreCase("Love")) {
+                System.out.println("Recommended Love Movies :");
+                for (String movie : Love) {
+                    System.out.println(movie);
+                }
+            } else{
+                System.out.println("Sorry ,No Recommendations available for that genre. Please choose from the given Recommendations.");
+             }
+            
         }
+        sc.close();
     }
 }
+
